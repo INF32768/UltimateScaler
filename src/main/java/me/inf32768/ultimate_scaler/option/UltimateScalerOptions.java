f2fafdf9dc5516b2142d8c1280e8c4581bf9b059
INF32768 <3478658394@qq.com>
2026-01-31 21:22:22 +0800

    chore: introduce the Shadow Plugin to automate dependency library packaging
    so the `shadowed` package in the src is removed

--- src/main/java/me/inf32768/ultimate_scaler/option/UltimateScalerOptions.java
+++ src/main/java/me/inf32768/ultimate_scaler/option/UltimateScalerOptions.java
@@ -8,7 +8,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
-import shadowed.com.moandjiezana.toml.Toml;
+import com.moandjiezana.toml.Toml;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
