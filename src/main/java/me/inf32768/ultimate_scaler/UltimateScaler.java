42c998ff3c1721c89331e46256718aebdcb41d56
INF32768 <3478658394@qq.com>
2026-03-08 17:01:03 +0800

    fix: delete a useless variable in the main class
    I don't even know what this variable is used for.

--- src/main/java/me/inf32768/ultimate_scaler/UltimateScaler.java
+++ src/main/java/me/inf32768/ultimate_scaler/UltimateScaler.java
@@ -6,9 +6,7 @@ import me.inf32768.ultimate_scaler.option.KeyBindings;
 import me.inf32768.ultimate_scaler.option.UltimateScalerOptions;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.server.MinecraftServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,12 +17,10 @@ public class UltimateScaler implements ModInitializer {
 public static final boolean IS_RUNNING_ON_CLIENT = FabricLoader.getInstance().getEnvironmentType().equals(EnvType.CLIENT);
 public static final boolean IS_CLOTH_CONFIG_PRESENT = FabricLoader.getInstance().isModLoaded("cloth-config2");
 public static final boolean IS_FABRIC_API_PRESENT = FabricLoader.getInstance().isModLoaded("fabric-api");
-public static MinecraftServer serverInstance;
 public static final Logger LOGGER = LoggerFactory.getLogger("ultimate_scaler");
 
     @Override
     public void onInitialize() {
-        ServerLifecycleEvents.SERVER_STARTED.register(server -> serverInstance = server);
         if (IS_RUNNING_ON_CLIENT) {
             if (IS_FABRIC_API_PRESENT) {
                 KeyBindings.init();
