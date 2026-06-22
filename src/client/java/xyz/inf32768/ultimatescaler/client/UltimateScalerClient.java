d05d255c862f0913d74e0e44360008fa926f93bd
INF32768 <3478658394@qq.com>
2026-06-22 13:38:06 +0800

    chore: initialize modern Fabric architecture with mojmap

---
+++ src/client/java/xyz/inf32768/ultimatescaler/client/UltimateScalerClient.java
@@ -0,0 +1,10 @@
+package xyz.inf32768.ultimatescaler.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UltimateScalerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
